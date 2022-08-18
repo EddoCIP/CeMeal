f439546c24f00484c6d0fb3c50f32084704065ad
Eddo Careera Iriyanto Putra <eddocareera.i.p@gmail.com>
2022-08-18 16:24:13 +0700

    Initial Commit

---
+++ CeMeal/CeMealApp.swift
@@ -0,0 +1,20 @@
+//
+//  CeMealApp.swift
+//  CeMeal
+//
+//  Created by Eddo Careera Iriyanto Putra on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CeMealApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
